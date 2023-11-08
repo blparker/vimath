6638ed49fb01d26f510acb125095f8dbd385674c
Bryan Parker <bryan.parker@gmail.com>
2023-11-08 10:05:59 -0600

    Adds NumberLine object

--- src/shapes/number_line.ts
+++ src/shapes/number_line.ts
@@ -1,8 +1,8 @@
-import { X_TICKS, Y_TICKS, Range, Point, VAlign, HAlign } from "../base";
-import { ComposableShape } from "./composed_shape";
+import { X_TICKS, Y_TICKS, Range, Point, VAlign, HAlign } from '../base';
+import { ComposableShape } from './composed_shape';
 import * as math from '../math';
-import { Line } from "./base_shapes";
-import { Text } from "./text";
+import { Line } from './base_shapes';
+import { Text } from './text';
 
 
 type NumberLineArgs = {
@@ -16,6 +16,7 @@ type NumberLineArgs = {
     tickStep?: number;
     rotation?: number;
     center?: Point;
+    showZero?: boolean;
 };
 
 
@@ -25,11 +26,12 @@ const defaultNumberLineArgs = {
     showTicks: true,
     showLabels: true,
     tickSize: 0.2,
-    tickLabelStandoff: 0.1,
+    tickLabelStandoff: 0.4,
     labelSize: 20,
     tickStep: 1,
     rotation: 0,
     center: [0, 0] as Point,
+    showZero: true,
 } as const;
 
 
@@ -43,10 +45,11 @@ export class NumberLine extends ComposableShape {
     private readonly labelSize: number;
     private readonly tickStep: number;
     private readonly rotation: number;
-    // private readonly center: Point;
+    private readonly centerPoint: Point;
+    private readonly showZero: boolean;
 
 
-    constructor({ length, range, showTicks, showLabels, tickSize, tickLabelStandoff, labelSize, tickStep, rotation, center }: NumberLineArgs = {}) {
+    constructor({ length, range, showTicks, showLabels, tickSize, tickLabelStandoff, labelSize, tickStep, rotation, center, showZero }: NumberLineArgs = {}) {
         super();
 
         this.length = length ?? defaultNumberLineArgs.length;
@@ -58,7 +61,8 @@ export class NumberLine extends ComposableShape {
         this.labelSize = labelSize ?? defaultNumberLineArgs.labelSize;
         this.tickStep = tickStep ?? defaultNumberLineArgs.tickStep;
         this.rotation = rotation ?? defaultNumberLineArgs.rotation;
-        // this.center = center ?? defaultNumberLineArgs.center;
+        this.centerPoint = center ?? defaultNumberLineArgs.center;
+        this.showZero = showZero ?? defaultNumberLineArgs.showZero;
     }
 
     compose(): ComposableShape {
@@ -71,25 +75,13 @@ export class NumberLine extends ComposableShape {
             ];
         };
 
-        // const startLine = [-this.length / 2, 0];
-        // const endLine = [this.length / 2, 0];
-
-        // let [sX, sY] = [-this.length / 2, 0];
-        // let [eX, eY] = [this.length / 2, 0];
-
-        // [sX, sY] = [
-        //     sX * Math.cos(this.rotation) - sY * Math.sin(this.rotation),
-        //     sX * Math.sin(this.rotation) + sY * Math.cos(this.rotation)
-        // ];
-
-        // [eX, eY] = [
-        //     eX * Math.cos(this.rotation) - eY * Math.sin(this.rotation),
-        //     eX * Math.sin(this.rotation) + eY * Math.cos(this.rotation)
-        // ];
+        const translate = (p: Point): Point => {
+            return [p[0] + this.centerPoint[0], p[1] + this.centerPoint[1]];
+        };
 
         this.add(new Line({
-            from: rotateAboutCenter([-this.length / 2, 0]),
-            to: rotateAboutCenter([this.length / 2, 0]),
+            from: translate(rotateAboutCenter([-this.length / 2, 0])),
+            to: translate(rotateAboutCenter([this.length / 2, 0])),
         }));
 
         if (!this.showTicks && !this.showLabels) {
@@ -110,6 +102,10 @@ export class NumberLine extends ComposableShape {
         }
 
         for (let i = start; i <= end; i += this.tickStep) {
+            if (i === 0 && !this.showZero) {
+                continue;
+            }
+
             const p = math.distance(this.range[0], i) / math.range(this.range);
             const x = math.lerp(-this.length / 2, this.length / 2, p);
 
@@ -119,38 +115,29 @@ export class NumberLine extends ComposableShape {
         const numDecimals = Math.min(math.mode(ticks.map(l => math.numDecimals(l[0]))), 5);
         for (const [label, x] of ticks) {
             if (this.showTicks) {
-                console
-                this.add(new Line({ from: rotateAboutCenter([x, -ht]), to: rotateAboutCenter([x, ht]) }));
+                this.add(new Line({ from: translate(rotateAboutCenter([x, -ht])), to: translate(rotateAboutCenter([x, ht])) }));
             }
 
             if (this.showLabels) {
-                let [rX, rY] = rotateAboutCenter([x, 0]);
+                let [rX, rY] = translate(rotateAboutCenter([x, 0]));
 
-                // let align: HAlign = 'center';
-                // let baseline: VAlign = 'top';
                 let align: HAlign = 'center';
                 let baseline: VAlign = 'middle';
-                let horizShift = 0;
-                let vertShift = -ht - this.tickLabelStandoff;
+                let tickDirection = 1;
 
-                if (this.rotation >= Math.PI / 4 && this.rotation <= 3 * Math.PI / 4) {
-                    // align = 'right';
-                    // baseline = 'middle';
-                    // vertShift = 0;
-                    // horizShift = -ht - this.tickLabelStandoff;
+                if (this.rotation > Math.PI / 4 && this.rotation < 3 * Math.PI / 4) {
+                    tickDirection = -1;
+                    align = 'right';
+                    baseline = 'middle';
                 }
 
-                horizShift = vertShift = 0;
-
-                const lX = 0.4 * Math.sin(this.rotation);
-                const lY = 0.4 * Math.cos(this.rotation);
+                const lX = rX + tickDirection * this.tickLabelStandoff * Math.sin(this.rotation);
+                const lY = rY - tickDirection * this.tickLabelStandoff * Math.cos(this.rotation);
 
                 this.add(new Text({
                     text: label.toFixed(numDecimals),
-                    // x: rX + horizShift,
-                    // y: rY + vertShift,
-                    x: rX + lX,
-                    y: rY - lY,
+                    x: lX,
+                    y: lY,
                     align,
                     baseline,
                     size: this.labelSize
---
+++ test/shapes/number_line.test.ts
@@ -0,0 +1,97 @@
+import { X_TICKS } from '../../src/base';
+import { Line } from '../../src/shapes/base_shapes';
+import { Text } from '../../src/shapes/text';
+import { NumberLine } from '../../src/shapes/number_line';
+import { expectArraysClose } from '../utils';
+
+
+describe('number line module', function() {
+    test('should create a number line with defaults', () => {
+        const n = new NumberLine();
+        const cs = n.composedShapes();
+
+        // Labels are shown by default, so the width isn't _exactly_ X_TICKS, but slightly above
+        expect(n.width()).toBeCloseTo(X_TICKS, 0);
+        // 1 shape for line, X_TICKS + 1 for ticks, and S_TICKS
+        expect(cs.length).toEqual(1 + 2 * (X_TICKS + 1));
+        // Verify that there are ticks and labels
+        expect(cs[0]).toBeInstanceOf(Line);
+
+        for (let i = 0; i < X_TICKS; i += 2) {
+            expect(cs[i + 1]).toBeInstanceOf(Line);
+            expect(cs[i + 2]).toBeInstanceOf(Text);
+        }
+    });
+
+
+    test('should default to center and full width', () => {
+        const n = new NumberLine({ showLabels: false, showTicks: false });
+        const cs = n.composedShapes();
+
+        expect(n.width()).toEqual(X_TICKS);
+        expect(n.center()).toEqual([0, 0]);
+    });
+
+
+    test('should create with specified length', () => {
+        const n = new NumberLine({ length: 10, showLabels: false, showTicks: false });
+        const cs = n.composedShapes();
+
+        expect(n.width()).toEqual(10);
+    });
+
+
+    test('should create with range', () => {
+        const startRange = -20;
+        const endRange = 60;
+
+        const n = new NumberLine({ range: [startRange, endRange], showTicks: false });
+        const cs = n.composedShapes();
+
+        expect(cs[0]).toBeInstanceOf(Line);
+        for (let i = 0; i <= Math.abs(startRange) + Math.abs(endRange); i += 2) {
+            const l = cs[i + 1];
+
+            expect(l).toBeInstanceOf(Text);
+            expect((l as Text).text).toEqual(`${startRange + i}`);
+        }
+    });
+
+
+    test('should create with specified tick step', () => {
+        const length = 8;
+        const tickStep = 2;
+        const n = new NumberLine({ length, tickStep });
+        const cs = n.composedShapes();
+
+        // Width = 10, step size = 2, plus 1 for the end, resulting in 6 ticks
+        // const numTicks = length / 2 + 1;
+        const startRange = -length / 2;
+
+        for (let i = 1; i < cs.length; i += 2) {
+            expect(cs[i]).toBeInstanceOf(Line);
+            expect(cs[i + 1]).toBeInstanceOf(Text);
+
+            const expectedX = startRange + (i - 1);
+            expect((cs[i] as Line).points()).toEqual([[expectedX, -0.1], [expectedX, 0.1]]);
+            expect((cs[i + 1] as Text).text).toEqual(`${expectedX}`);
+        }
+    });
+
+
+    test('should create with a specified center', () => {
+        const n = new NumberLine({ center: [1, 2], length: 6, showTicks: false, showLabels: false });
+        const cs = n.composedShapes();
+
+        // When centered at (0, 0), the left is at (-3, 0) and the right is at (3, 0). Changing the center to (1, 2) shifts the endpoints by (1, 2)
+        expect((cs[0] as Line).points()).toEqual([[-2, 2], [4, 2]]);
+    });
+
+
+    test('shoudl rotate by specified angle', () => {
+        const n = new NumberLine({ rotation: Math.PI / 2, length: 6, showTicks: false, showLabels: false });
+        const cs = n.composedShapes();
+
+        expectArraysClose((cs[0] as Line).points(), [[0, -3], [0, 3]]);
+    });
+});
