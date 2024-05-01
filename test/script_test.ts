a8f5b4ee09042131780fa50362e568e1c9a4aa6c
Bryan Parker <bryan.parker@gmail.com>
2024-05-01 09:48:41 -0500

    Dot coloring

--- test/script_test.ts
+++ test/script_test.ts
@@ -336,7 +336,7 @@ class TestScene extends Scene {
         //     new Dot({ center: p4, color: Colors.green() }),
         // );
 
-        this.add(new Dot({ center: [0, 0], color: Colors.blue() }));
+        this.add(new Dot({ center: [0, 0], color: Colors.white(), lineColor: Colors.blue() }));
         this.add(new Dot().moveTo([0, 1]));
     }
 }
